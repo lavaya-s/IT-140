cf21575a37950d2a182be20da2201e0d90944c91
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-02-25 22:57:56 -0500

    Update '''Varying inputs'''.py

--- '''Varying inputs'''.py
+++ '''Varying inputs'''.py
@@ -4,11 +4,10 @@ def ints(args):#define a function to accept any number of arguments
     average=0#set the average to zero
     count=0#set the count to zero
     numbers = []#create empty list
-    for arg in args:
-        numbers.append(arg)
-    for arg in numbers:
-        count+=1
-        print(count)
+    for arg in args:#iterate over the list
+        numbers.append(arg)#append the argument to the end of the list
+    for arg in numbers:#iterate over the list
+        count+=1#increment the count for the floor division of average
         average += arg#add the numbers together
         if arg > max_int:#check to see if it is greater
          max_int=arg#assign the greatest value
@@ -17,10 +16,9 @@ def ints(args):#define a function to accept any number of arguments
 values = []#create empty list to store data
 value = input().split(' ')#take input from user
 #iterate over user input to store into the empty list
-for v in value:
+for v in value:#iterate over the list
     if v != ' ':#ignore spaces
-        values.append(int(v))
-        print(values)
+        values.append(int(v))#add the values to the end of the list
 #callthe function to add all arguments passed through
 ints(values)#remember to unpck the list to create all the arguments
 print(average,max_int)#print the results
