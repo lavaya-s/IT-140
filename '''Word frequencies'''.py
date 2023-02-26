f20bfb4129fe0b706e665f39563e760aea38211e
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-02-25 22:58:08 -0500

    documented code

--- '''Word frequencies'''.py
+++ '''Word frequencies'''.py
@@ -1,5 +1,6 @@
 '''Word frequencies'''
-sentence=input().split()
-for word in sentence:
-    num=len([1 for w in sentence if w == word])
-    print(f'{word} {num}')
+sentence=input().split()#take input from the user and split into a list
+for word in sentence:#iterate over the list
+    num=len([1 for w in sentence if w == word]) #create a list of tallies on the condition that it is equal to the current element
+                                                #then assign it's total length to the variable for frequency
+    print(f'{word} {num}')#print the word and frequencuy of the word in the list
