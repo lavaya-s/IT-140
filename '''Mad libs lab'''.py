de6ea99d048048990798d4b1cbd441729548d37c
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-03-04 18:23:24 -0500

    Update '''Mad libs lab'''.py
    clean up

--- '''Mad libs lab'''.py
+++ '''Mad libs lab'''.py
@@ -1,25 +1,25 @@
 ####################################################################
 words = {}#create dictionaries to hold the keys to inputs
-numbers={}
+numbers={}#create empty dict for nums
 game = True#bool flag for the while loop
 count = 0#start counting iterations
 def UserText():#functiion to take user input into dictionaries
     global game, num, noun, count, words, numbers#declare variables
-    try:
+    try:#catch End of file Errors
         noun = input()#variable for user input
-        num = int(input())
+        num = int(input())#take input for an integer
         words.update({count:noun})#put the values into the dictionaries 
         numbers.update({count:num})#count is the key for range in the for loop
         if 'quit' in words.values():#stops the while loop when the user enters quit
-            game = False
+            game = False#set Boolean value to False
     except EOFError:#this happens once the zybooks input completes
         game = False#this prevents the while loop from continuing
 def MadLib(num,noun):#takes args for numbers and nouns
     #format to put user input into the string
     if noun != 'quit':#doesn't print the quit statement
         print(f'Eating {num} {noun} a day keeps the doctor away.')
-while game == True:
-    UserText()
+while game == True:#LOOPS while condition is true. game variable BOOL is effectively ON
+    UserText()#call the user text function defined earlier
     count += 1#count after so no index out of range error
 #runs only once the while loop completes
 for element in range(count):#this runs for each time the input was taken
