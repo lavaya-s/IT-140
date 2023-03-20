d440982eb7bf0b2f3756dc70a96f9fe58fd4e3f7
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-03-19 23:31:04 -0400

    Update NameAge.py

--- NameAge.py
+++ NameAge.py
@@ -2,11 +2,9 @@
 from datetime import datetime
 def NameAge():
 #ask user for name
- #What is your name? Amanda
     user_name = input('What is your name? ')
     user_name.strip()#strip any whitespace entered by the user
 #ask user for age
-#How old are you? 15
     user_age = int(input('How old are you? '))
 #take down what year it is
 #Use dot notation
@@ -16,7 +14,5 @@ def NameAge():
 #calculate year the user was born using current year minus age
     year_born = year - user_age
     year_born = str(year_born).strip()#strip any whitespace entered by the user
-#sample output:
-#Hello Amanda! You were born in 2005.
     print(f'Hello {user_name}! You were born in {year_born}.')
 NameAge()
