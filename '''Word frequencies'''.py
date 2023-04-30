28d55158793b35ea80a41b7559dd9bef2cef8b20
lavaya-s <105933477+lavaya-s@users.noreply.github.com>
2023-04-30 16:44:49 -0400

    clarity
    added text to input function for clarity

--- '''Word frequencies'''.py
+++ '''Word frequencies'''.py
@@ -1,6 +1,6 @@
 '''Word frequencies'''
-sentence=input().split()#take input from the user and split into a list
+sentence=input('Please type as much as you would like.').split()#take input from the user and split into a list
 for word in sentence:#iterate over the list
     num=len([1 for w in sentence if w == word]) #create a list of tallies on the condition that it is equal to the current element
                                                 #then assign it's total length to the variable for frequency
-    print(f'{word} {num}')#print the word and frequencuy of the word in the list
+    print(f'{word} {num}')#print each word and frequencuy of the word in the list
